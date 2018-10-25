fa6cd3af4806d3e0f8508129826be73bc1a1c66b
JCacheng <15007006602@163.com>
2018-10-25 20:55:56 +0800

    列表渲染

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-(function (window, Vue,) {
-	'use strict';
+(function (window, Vue, undefined) {
+	
+	new Vue({
+		el: '#app',
+		data: {
+			dataList: JSON.parse(window.localStorage.getItem('dataList')) || []
+		},
+		watch: {
+			dataList: {
+				handler (newArr) {
+					window.localStorage.setItem('dataList', JSON.stringify(newArr));
+				},
+				deep: true
+			}
+		},
+		directives: {
+			focus: {
+				inserted (el) {
+					el.focus();
+				}
+			}
+		}
 
-	// Your starting point. Enjoy the ride!
-
+	})
 
 
 })(window, Vue);
