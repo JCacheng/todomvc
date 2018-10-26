2ae20c0fc7159ce46fca0a477819b9105d5b5303
JCacheng <15007006602@163.com>
2018-10-26 17:49:37 +0800

    hashchange事件显示三大列表，并解决删除显示切换的问题

--- js/app.js
+++ js/app.js
@@ -5,12 +5,16 @@
 		data: {
 			dataList: JSON.parse(window.localStorage.getItem('dataList')) || [],
 			newTodo: '',
-			beforeUpdate: {}
+			beforeUpdate: {},
+			activeBtn: 1,
+			showArr: []
 		},
 		watch: {
 			dataList: {
 				handler(newArr) {
 					window.localStorage.setItem('dataList', JSON.stringify(newArr));
+					// 判断当前显示的三大列表之一是否有可以显示数据  如果没有 跳转至所有显示
+					this.hashchange();
 				},
 				deep: true
 			}
@@ -36,7 +40,7 @@
 			},
 			//给当前li添加editing类名 显示编辑文本框
 			showEdit(index) {
-				this.$refs.show.forEach(item => { return item.classList.remove('editing'); });
+				this.$refs.show.forEach(item => item.classList.remove('editing'));
 				this.$refs.show[index].classList.add('editing');
 				this.beforeUpdate = JSON.parse(JSON.stringify(this.dataList[index]));
 			},
@@ -52,6 +56,34 @@
 				this.dataList[index].content = this.beforeUpdate.content;
 				this.$refs.show[index].classList.remove('editing');
 				this.beforeUpdate = {};
+			},
+			// 哈希change事件
+			hashchange() {
+				switch (window.location.hash) {
+					case '':
+					case '#/':
+						this.showAll();
+						this.activeBtn = 1;
+						break;
+					case '#/active':
+						this.activeAll(false);
+						this.activeBtn = 2;
+						break;
+					case '#/completed':
+						this.activeAll(true);
+						this.activeBtn = 3;
+						break;
+				}
+			},
+			// 显示所有 创建一个显示的数组
+			showAll() {
+				this.showArr = this.dataList.map(() => true)
+			},
+			// 修改显示的数组使用 就是显示活动的
+			activeAll(boo) {
+				this.showAll = this.dataList.map(item => item.isFinish === boo);
+				// 在切换到active等页面显示列表的时候 使用全部删除同样需要判断当前还是否有可显示的数据 没有就跳转
+				if (this.dataList.every(item => item.isFinish === !boo)) return window.location.hash = '#/';
 			}
 		},
 		//计算属性
@@ -69,11 +101,19 @@
 				}
 			}
 		},
+		// 自定义指令
 		directives: {
 			focus: {
 				inserted(el) {
 					el.focus();
 				}
+			}
+		},
+		// 生命周期 在最一开始就执行 这个钩子函数在创建了数据后还没有开始渲染页面时触发 如data methods装载在vue实例后就执行
+		created() {
+			this.hashchange();
+			window.onhashchange = function () {
+				this.hashchange();
 			}
 		}
 
